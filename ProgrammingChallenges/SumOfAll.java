c91c4fcfcf437cccacbf3ad54adbee8f9a071515
P4ndaaaa123 <mulano.josh@gmail.com>
2025-11-22 17:29:57 +0800

    Update SumOfAll.java

--- ProgrammingChallenges/SumOfAll.java
+++ ProgrammingChallenges/SumOfAll.java
@@ -11,7 +11,6 @@ public class SumOfAll {
         int count = 5;
         int sum = 0;
 
-
         for(int i = 1; i<=count; i++){
 
             System.out.print("Enter a number " + i + ": ");
@@ -21,28 +20,6 @@ public class SumOfAll {
         }
 
         System.out.println("The Total sum is " + sum);
-
-        
-        
-        // ArrayList <Integer> arrayList = new ArrayList<>();
-        
-        // while (true) {
-
-        //     System.out.print("Enter a number:");
-        //     int num1 = sc.nextInt();
-        //     arrayList.add(num1);
-        //     System.out.print("Continue? ");
-        //     String repeat = sc.next();
-
-        //     if (repeat.equalsIgnoreCase("N") && !repeat.equalsIgnoreCase("Y")) {
-        //         break;
-        //     }
-        // }
-
-        // for(int x : arrayList){
-        //     System.out.println(x);
-        // }
-        
 
     }
 }
